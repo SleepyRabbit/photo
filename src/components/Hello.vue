70fb40ba27d61f578f3a39b2d7367b9cf8de3a49
= <houxing2846@163.com>
2017-06-26 18:20:41 +0800

    A photo demo upload on 2017.6.26

---
+++ src/components/Hello.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+  <p>this is a test!</p>
+    <button @click="uploadFile">upload</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+      uploadFile: function () {
+        let form = new FormData();
+        form.append('photo',"./Hello.vue", "Hello.vue" );
+        this.$http.post("127.0.0.1:8080", form, {
+
+        });
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
