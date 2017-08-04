2a75acd401d87f048d75a8e9aef9920f4efc24b8
= <houxing2846@163.com>
2017-08-04 18:46:11 +0800

    update 2017.8.4

--- src/components/upload.vue
+++ src/components/upload.vue
@@ -1,17 +1,9 @@
 <template>
   <div class="upload">
-  <p>this is a test!</p>
-    <!--<form action="" enctype="multipart/form-data">-->
-      <input type="file" @change="onFileChange" accept="image/*">
-      <button @click="uploadFile">Submit</button>
-      <!--</form>-->
-
-    <!--<form action="/api/upload" method="post" enctype="multipart/form-data">-->
-      <!--<label>Please select photos to update: </label><input name="photo" type="file" multiple></input>-->
-      <!--<input type="submit">-->
-    <!--</form>-->
-    <p>sss</p>
-
+  <p>Please select the photos you want to upload!</p>
+  <input type="file" @change="onFileChange" accept="image/*" multiple>
+  <button @click="uploadFile">Submit</button>
+  <h4>{{ tip }}</h4>
   </div>
 </template>
 
@@ -22,35 +14,44 @@ export default {
   data () {
     return {
       filelength: 0,
-      filelist: null
+      filelist: [],
+      tip: "",
     }
   },
   methods: {
       onFileChange: function (e) {
         this.filelist  = e.target.files || e.dataTransfer.files;
-        if (!this.filelist.length)
-          return;
-
         console.log("File change!");
+        this.tip = "";
       },
       uploadFile: function () {
-        var formData = new FormData();
-        let file = this.filelist[0];
-        // append string
-//        formData.type = "multipart/form-data"
-        formData.append("file", file);
-        formData.append('user', 'bar');
+        let file_num = this.filelist.length;
+        // 检查用户是否选择了文件
+        if (!file_num) {
+            this.tip = "No file selected!";
+            return;
+        }
 
+        // 构造formdata用于文件传输
+        let formData = new FormData();
+
+        for(let i = 0; i < file_num; i++) {
+          formData.append("file", this.filelist[i]);
+        }
+////        formData.type = "multipart/form-data"
+////        formData.append("file", file);
+        formData.append('user', 'bar');
+//
         this.$http.post("/api/upload", formData)
-          .then( res => {
-            // success callback
-            console.log("File sent...");
-            console.log(res.data);
-          }, res => {
-            // error callback
-            console.log("Error occurred...");
-            console.log(res);
-          });
+        .then( res => {
+          // success callback
+          console.log("File sent...");
+          console.log(res.data);
+        }, res => {
+          // error callback
+          console.log("Error occurred...");
+          console.log(res);
+        });
       }
   }
 }
